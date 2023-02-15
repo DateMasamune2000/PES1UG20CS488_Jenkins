b79d516030b677f31107b3f124e2fbea8be1f4d1
Varun Girimaji <vgirimaji@gmail.com>
2023-02-15 19:47:30 +0530

    dank memes

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "You expected something, but it was I, DIO!\n";
+  cout << "What a beatuful Morioh! *chew*\n";
   return 0;
 }
